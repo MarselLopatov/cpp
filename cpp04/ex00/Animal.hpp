dedaf64b65894130692c2852716f473b90606acd
Cerebra Doria <cdoria@at-d1.msk.21-school.ru>
2022-10-19 15:23:30 +0300

    module 04 start

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdoria <cdoria@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 14:35:59 by cdoria            #+#    #+#             */
+/*   Updated: 2022/10/19 14:37:14 by cdoria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+private:
+	std::string	type;
+
+public:
+	Animal(/* args */);
+	~Animal();
+};
+
+#endif
